10591b4164fb92b63c56efb15c4eac26a492dcb5
1slander <hectorgomendez@gmail.com>
2023-06-30 09:51:59 +0100

    install MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 
 
@@ -7,6 +11,7 @@ function App() {
   return (
     <>
       <h1>hello world</h1>
+      
     </>
   )
 }
