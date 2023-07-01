8344586630ead5874d1b5ca97a639d34562c49fd
1slander <hectorgomendez@gmail.com>
2023-07-01 09:18:25 +0100

    created form

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,112 +1,64 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from "react";
+import { useState } from "react";
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("user");
 
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
-
-export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = { name, surname, email, password, role };
+    console.log("New user created", newUser);
+  };
+  const handleNameInpt = (e) => setName(e.target.value);
+  const handleSurnameInpt = (e) => setSurname(e.target.value);
+  const handleEmailInpt = (e) => setEmail(e.target.value);
+  const handlePasswordInpt = (e) => setPassword(e.target.value);
+  const handleRoleInpt = (e) => {
+    e.target.checked ? setRole("teacher") : setRole("user");
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+    <div className="signup">
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="">Name:</label>
+        <input type="text" name="name" value={name} onChange={handleNameInpt} />
+        <label htmlFor="">Surname:</label>
+        <input
+          type="text"
+          name="surname"
+          value={surname}
+          onChange={handleSurnameInpt}
+        />
+        <label htmlFor="">Email:</label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={handleEmailInpt}
+        />
+        <label htmlFor="">Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordInpt}
+        />
+        <label htmlFor="">Role:</label>
+        Are you a teacher?
+        <input
+          type="checkbox"
+          name="role"
+          value={role}
+          onChange={handleRoleInpt}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
   );
-}
+};
+
+export default SignUp;
