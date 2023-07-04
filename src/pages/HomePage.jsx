983e593ecaa3a09578e21b933e501788b8fdc372
1slander <hectorgomendez@gmail.com>
2023-07-04 16:34:24 +0100

    add login

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { Link } from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 const HomePage = () => {
+  // const { isLoggedIn, user } = useContext(AuthContext);
+
   return (
     <>
       <Navbar />
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,37 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import axios from "axios";
 import { Button } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
-const URL = "";
+const URL = import.meta.env.VITE_API_URL;
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+  const { storeToken, authenticateUser } = useContext(AuthContext);
 
   const handleEmail = (e) => setEmail(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
   const handleLoginSubmit = (e) => {
     e.preventDefault();
     const requestBody = { email, password };
+
+    axios
+      .post(`${URL}/auth/login`, requestBody)
+      .then((response) => {
+        console.log("JWT TOKEN", response.data.authToken);
+        storeToken(response.data.authToken);
+        authenticateUser();
+        navigate("/dashboard/:userId");
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
 
     console.log("You have log in", requestBody);
   };
@@ -42,6 +61,10 @@ const Login = () => {
           Login
         </Button>
       </form>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      <p>Don't have an account yet?</p>
+      <Link to={"/signup"}> Sign Up</Link>
     </div>
   );
 };
