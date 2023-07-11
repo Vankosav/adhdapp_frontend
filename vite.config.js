d2307fb1a61d12311128c14e78df86f6424d2757
1slander <hectorgomendez@gmail.com>
2023-07-11 18:50:52 +0100

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
 // vite.config.js
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
 
-export default {
 export default {
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5005',
+      "/api": {
+        target: "http://localhost:5005",
         changeOrigin: true,
       },
     },
   },
   build: {
-    base: '/path/to/your/app',
+    base: "/path/to/your/app",
   },
 };
