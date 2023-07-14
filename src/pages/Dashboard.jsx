879e79e0dae9b550fe95da2f3d0bc995ca50fe7f
[Ivana Nedeljkov] <[ivkaned@gmail.com]>
2023-07-14 18:16:06 +0200

    CRUD done, check routes for dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,14 +2,14 @@ import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { useEffect, useState } from "react";
 import Notes from "../components/Notes";
-import NotesList from "../components/NotesList";
+
 
 const Dashboard = () => {
   return (
     <>
       <Navbar />
       <Notes />
-      <NotesList />
+    
      
     </>
   );
