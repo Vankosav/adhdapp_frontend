fa73581e96e7c7939ef1260b78ca1f1a1b3b1b03
1slander <hectorgomendez@gmail.com>
2023-07-19 19:27:45 +0100

    add back button

---
+++ src/context/PomodoroSettings.context.jsx
@@ -0,0 +1,5 @@
+import react from "react";
+
+const PomodoroSettingsContext = react.createContext({});
+
+export default PomodoroSettingsContext;
---
+++ src/pages/Settings.jsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import ReactSlider from "react-slider";
+import PomodoroSettingsContext from "../context/PomodoroSettings.context";
+
+const Settings = () => {
+  return <div>Settings</div>;
+};
+
+export default Settings;
