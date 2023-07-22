eade09dbc2bb9101a35cb86fcc2a4567101e9c9c
1slander <hectorgomendez@gmail.com>
2023-07-22 12:27:04 +0100

    fix css bugs

--- src/components/Pomodoro/PauseBtn.jsx
+++ src/components/Pomodoro/PauseBtn.jsx
@@ -1,6 +1,6 @@
 const PauseBtn = (props) => {
   return (
-    <button {...props}>
+    <button className="pomodoroBtn" {...props}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
